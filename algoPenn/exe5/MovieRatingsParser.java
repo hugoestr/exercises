e3978dcb7bd221db155c6cc97f896aceacda68fc
Hugo Estrada <Hugo.Estrada@afge.org>
2017-10-06 11:16:52 -0400

    finish exercise 5

--- algoPenn/exe5/MovieRatingsParser.java
+++ algoPenn/exe5/MovieRatingsParser.java
@@ -11,10 +11,36 @@ import java.util.TreeMap;
 public class MovieRatingsParser {
 
 	public static TreeMap<String, PriorityQueue<Integer>> parseMovieRatings(List<UserMovieRating> allUsersRatings) {
+	 TreeMap<String, PriorityQueue<Integer>> result = new	TreeMap<String, PriorityQueue<Integer>>();
 		
-		/* IMPLEMENT THIS METHOD! */
+   if (allUsersRatings == null || allUsersRatings.isEmpty()){
+    return result;
+   }
+   
+   PriorityQueue<Integer> priority;
+
+   for (UserMovieRating review : allUsersRatings){
+      if (review == null || 
+          review.getMovie() == null || 
+          review.getMovie() == "" ||
+          review.getUserRating() < 0){
+          continue;
+      }
+
+      String title = review.getMovie().toLowerCase();
+
+      if (!result.containsKey(title)){
+         priority = new PriorityQueue<Integer>();
+         priority.add(review.getUserRating());
+
+         result.put(title, priority);
+      } else {
+         priority = result.get(title);
+         priority.add(review.getUserRating());
+      }
+   }
 		
-		return null; // this line is here only so this code will compile if you don't modify it
+		return result; 
 	}
 
 }
--- algoPenn/exe5/MovieRatingsProcessor.java
+++ algoPenn/exe5/MovieRatingsProcessor.java
@@ -7,28 +7,94 @@
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.TreeMap;
+import java.util.Map;
+import java.util.ArrayList;
 
 
 public class MovieRatingsProcessor {
 
 	public static List<String> getAlphabeticalMovies(TreeMap<String, PriorityQueue<Integer>> movieRatings) {
+	  ArrayList<String> result = new ArrayList<String>();
+
+    if (movieRatings == null){
+      return result;
+    }
+
+      for (Map.Entry<String, PriorityQueue<Integer>> item : movieRatings.entrySet()){
+        if (item != null && item.getKey() != null && item.getKey() != ""){
+          result.add(item.getKey());
+        }
+      }
 		
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
+		return result; 
 	}
 
 	public static List<String> getAlphabeticalMoviesAboveRating(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
+		ArrayList<String> result = new ArrayList<String>();
+
+    if (movieRatings == null){
+      return result;
+    }
+  
+    PriorityQueue<Integer> queue;
+
+    for (Map.Entry<String, PriorityQueue<Integer>> item : movieRatings.entrySet()){
+      if (item != null && item.getKey() != null && item.getKey() != ""){
+        queue = item.getValue();
+
+        if (queue.peek() > rating){ 
+          result.add(item.getKey());
+        }
+      }
+    }
 		
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
+		return result;
 	}
 	
 	public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
-		
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
+	  TreeMap<String, Integer> result = new TreeMap<String, Integer>();
+    ArrayList delete = new ArrayList();  
+	
+    if (movieRatings == null || movieRatings.isEmpty()){
+      return result;
+    }
+
+    PriorityQueue<Integer> queue;
+    
+    for (Map.Entry<String, PriorityQueue<Integer>> item : movieRatings.entrySet()){
+      
+      if (item != null && 
+          item.getKey() != null 
+          && item.getKey() != ""){
+        
+        queue = item.getValue();
+
+        int erased = 0;
+
+        while (queue.size() > 0 && queue.peek() < rating){
+          queue.poll();
+          erased++;  
+        }
+
+        if (erased > 0){
+          result.put(item.getKey(), erased);
+        }
+
+        if (queue.size() == 0){
+          delete.add(item.getKey());
+        }
+      }
+    }
+
+    deleteItems(movieRatings, delete);
+
+		return result; 
 	}
+
+  private static void deleteItems(TreeMap<String, PriorityQueue<Integer>> movieRatings, List<String> toDelete){
+    for (String key : toDelete){
+      movieRatings.remove(key);
+    }
+  }
+  
 }
